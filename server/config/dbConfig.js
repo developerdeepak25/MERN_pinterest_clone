da20727bacec295c0212023285c6df4d99eaa1f6
developer_deepak <developer.deepak25@gmail.com>
2024-01-24 15:34:36 +0530

    modified config/dbconfig.js

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -3,12 +3,21 @@
 // Sample database configuration
 const mongoose = require("mongoose");
 
-const URI = process.env.MONGODB_URI
+const URI = process.env.MONGODB_URI;
 
 const connectDB = async () => {
   try {
     // Replace 'mongodb://localhost:27017/pinterest_clone' with your actual MongoDB URL
-    await mongoose.connect(URI);
+    // await mongoose.connect(URI);
+    mongoose
+      .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then(() => {
+        console.log("Connected to MongoDB");
+        // Continue with your application logic
+      })
+      .catch((error) => {
+        console.error("Error connecting to MongoDB:", error.message);
+      });
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
